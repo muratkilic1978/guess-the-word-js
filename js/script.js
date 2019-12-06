04ce28db96df311437f8e04224e0e4b3dd91149c
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-06 22:04:58 +0100

    gameplay inside script.js was updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 const message = document.querySelector(".message");
 const guess = document.querySelector("input");
 const button = document.querySelector("button");
+let inPlay = false;
+const randomWords = ["javascript","website","html","css","course","programming","document"];
+
 
 button.addEventListener("click", function(){
-    console.log("Button was clciked");
+    console.log("Button was clicked");
 });
+console.log(inPlay);
+if(!inPlay){
+    inPlay = true;
+    console.log(inPlay);
+    button.innerHTML = "Guess";
+    //guess.style.display = "inline-block";
+    guess.classList.toggle("hidden");
+}

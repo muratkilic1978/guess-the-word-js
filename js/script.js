605ec2264ef8cc5ff3fce1219baa79b692412cba
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-06 22:15:12 +0100

    random array with words was created

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const randomWords = ["javascript","website","html","css","course","programming",
 
 button.addEventListener("click", function(){
     console.log("Button was clicked");
-});
+
 console.log(inPlay);
 if(!inPlay){
     inPlay = true;
@@ -15,4 +15,18 @@ if(!inPlay){
     button.innerHTML = "Guess";
     //guess.style.display = "inline-block";
     guess.classList.toggle("hidden");
+    console.log(createWord());
+    }
+});
+
+function createWord(){
+    let randomIndex = Math.floor(Math.random() * randomWords.length);
+    console.log(randomIndex);
+    let tempWord = randomWords[randomIndex];
+    console.log(tempWord.split(""));
+    return tempWord;
+}
+
+function randomArray(arr){
+    
 }
